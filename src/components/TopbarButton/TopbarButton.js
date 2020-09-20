f71fe81a2c926df13e68ffbab99426a2d3ea318c
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-09-20 10:26:31 -0300

    Removed offsetX from javascript.svg

--- src/components/TopbarButton/TopbarButton.js
+++ src/components/TopbarButton/TopbarButton.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './TopbarButton.css';
 
+let nCalls = 0;
+
 const TopbarButton = ({ Svg, text, large=false }) => {
   const [isHovering, setIsHovering] = useState(false);
   const handleEnter = () => setIsHovering(true);
   const handleLeave = () => setIsHovering(false);
-
+  nCalls++;
+  console.log(`TopbarButton call n°: ${nCalls}`);
   return (
     <button 
       className='topbarbutton' 
