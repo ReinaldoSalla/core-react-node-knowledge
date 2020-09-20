a8b7a2f1f7709f7fd3338712ed7617322afb0b78
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-09-20 10:00:59 -0300

    Added TopbarButton hover effect

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import TopbarButton from '../TopbarButton';
+import { ReactComponent as ContentsSvg } from '../../assets/icons/contents.svg'
+import { ReactComponent as JavaScriptSvg } from '../../assets/icons/javascript.svg';
+import { ReactComponent as SearchSvg } from '../../assets/icons/search.svg';
+import { ReactComponent as ProfileSvg } from '../../assets/icons/profile.svg';
 import './Topbar.css';
 
 const Topbar = () => {
   return (
-    <header className='topbar'>
-      topbar
+    <header>
+      <nav className='topbar'>
+        <TopbarButton Svg={ContentsSvg} text='Contents' />
+        <TopbarButton Svg={JavaScriptSvg} text='Home' large/>
+        <TopbarButton Svg={SearchSvg} text='Search' large/>
+        <TopbarButton Svg={ProfileSvg} text='Profile' />
+      </nav>
     </header>
   );
 };
