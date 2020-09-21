9c77c114906a0ad18bdd1cb2e973c0263d8aaa5d
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-09-21 11:09:18 -0300

    Added the initial structure for the Courosel

--- src/components/Courosel/Courosel.js
+++ src/components/Courosel/Courosel.js
@@ -4,7 +4,18 @@ import './Courosel.css';
 const Courosel = () => {
   return (
     <div className='courosel'>
+      <h1 className='courosel-title'>
+        JavaScript Guides
+      </h1>
+      <h2 className='courosel-description'>
+        Data processing and asyncronous programming
+      </h2>
+      <div className='courosel-check'>
 
+      </div>
+      <div className='courosel-inputs'>
+        
+      </div>
     </div>    
   );
 };
