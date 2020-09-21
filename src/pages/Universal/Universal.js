64b1cbedc214e4a0055767fea4e665dc35807b79
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-09-21 10:27:12 -0300

    Added the Footer

--- src/pages/Universal/Universal.js
+++ src/pages/Universal/Universal.js
@@ -4,6 +4,7 @@ Universal component rendered throughout the whole app.
 
 import React from 'react';
 import Topbar from '../../components/Topbar';
+import Footer from '../../components/Footer';
 import RouteParser from '../../routes/RouteParser';
 import routesConfig from '../../routes/routes-config';
 
@@ -21,6 +22,7 @@ const Universal = () => {
           content #{index+1}
         </div>
       ))}
+      <Footer />
     </>
   );
 };
