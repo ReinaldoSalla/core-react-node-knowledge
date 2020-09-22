5284c5e12e74a5fd40df8cccc108417aba481c6d
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-09-22 09:19:11 -0300

    Added features in COurosel

--- src/components/Courosel/Courosel.js
+++ src/components/Courosel/Courosel.js
@@ -15,8 +15,17 @@ const Courosel = () => {
           Check JavaScript Tutorials
         </span>
       </div>
+
       <div className='courosel-inputs'>
-        <div className='courosel-inputs-timer' />
+        <div className='courosel-inputs-input'>
+          <span className='courosel-inputs-inputs-text'> JavaScript</span>
+        </div>
+        <div className='courosel-inputs-input'>
+          <span className='courosel-inputs-inputs-text'> React </span>
+        </div>
+        <div className='courosel-inputs-input'>
+          <span className='courosel-inputs-inputs-text'> Node </span>
+        </div>
       </div>
     </div>    
   );
