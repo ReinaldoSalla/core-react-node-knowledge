72a3f54b12b646bdf96437034acebed7d764fc25
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-11-18 17:01:50 -0300

    feature(eslint): tweeking the eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import 'fontsource-source-sans-pro/400-normal.css';
-import 'fontsource-source-sans-pro/400-italic.css';
-import 'fontsource-source-sans-pro/600-normal.css';
-import { GlobalStyle, globalTheme } from './App.styles';
-import Universal from './pages/Universal';
-import './App.css';
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={globalTheme}>
-        <GlobalStyle />
-        <Universal />
-      </ThemeProvider>
-    </BrowserRouter>
-  );
-};
-
-export default App;
-
-// import React, { useState, useEffect } from 'react';
-
-// let x;
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
+// import 'fontsource-source-sans-pro/400-normal.css';
+// import 'fontsource-source-sans-pro/400-italic.css';
+// import 'fontsource-source-sans-pro/600-normal.css';
+// import { GlobalStyle, globalTheme } from './App.styles';
+// import Universal from './pages/Universal';
+// import './App.css';
 
 // const App = () => {
-//   const [state, setState] = useState(null);
-
-//   useEffect(() => {
-//     console.log(state)
-//   }, []);
-
 //   return (
-//     <div onClick={() => {}}>div</div>
+//     <BrowserRouter>
+//       <ThemeProvider theme={globalTheme}>
+//         <GlobalStyle />
+//         <Universal />
+//       </ThemeProvider>
+//     </BrowserRouter>
 //   );
 // };
 
 // export default App;
+
+
+/* exposing errs with eslint */
+import React from 'react';
+
+// console.log('234');
+
+const App = () => {
+  
+  return (
+    // <div onClick={() => {}}>bad</div>
+    <div />
+  );
+};
+
+export default App;
