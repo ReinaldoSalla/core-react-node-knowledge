ad20fe0e43e1b4c52f458d98f53b492c8a00c2b0
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-12-12 09:07:25 -0300

    feat: added Content.data.ts

---
+++ src/pages/Content/Content.data.ts
@@ -0,0 +1,21 @@
+const data = {
+  loops: {
+    title: 'React - Simple State Management',
+    text: [
+      {
+        subtitle: '1. Intro',
+        paragraphsLinksCommandsCode: {
+          paragraph1: 'The guide aims to explore the useState hook from react. We\'l build a Navbar that re-renders a list of items when hovered over. We\'l also create a form and a table, wich will share data by lifting the state up.'
+        } 
+      },
+      {
+        subtitle: '2. Setup',
+        paragraphsLinksCommandsCode: {
+          paragraph1: 'Install the latest recommended version of',
+          link: 'Node.js',
+          paragraph2: 'To start the project, type in your terminal'
+        }
+      }
+    ]
+  }
+}
