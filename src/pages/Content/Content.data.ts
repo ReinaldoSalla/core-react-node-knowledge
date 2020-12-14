1da7f0eba8c77ca504b79af7039ae0cfe2e38800
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-12-14 17:12:32 -0300

    feat: variables for content.data

--- src/pages/Content/Content.data.ts
+++ src/pages/Content/Content.data.ts
@@ -5,57 +5,84 @@ onSubmit(e) {
     title: 'Developer',
     company: 'xyz' 
   };
-}`;
+}
+`;
+
+const title = `
+React - Simple State Management
+`;
+
+const subtitle1 = `
+Intro
+`;
+
+const paragraph1 = `
+The guide aims to explore the useState hook from
+react. We\'l build a Navbar that re-renders a list of items 
+when hovered over. We\'l also create a form and a table,
+wich will share data by lifting the state up.
+`;
+
+const subtitle2 = `
+Setup
+`;
+
+const paragraph2 = `
+Install the latest recommended version of *Node.js
+`;
+
+const paragraph3 = `
+Install the lastest recommended version of *Node.js* and later deno
+`;
+
+
+const commands = [
+'$ npx create-react-app progrtmp-simple-state-management',
+'$ cd progrtmp-simple-state-management',
+'$ npm run start'
+];
+
+const paragraph4 = `
+When running npm run start, React will lauch the app in 
+http://localhost:3000
+`;
+
+const subtitle3 = `
+Nabvar
+`;
+
+const paragraph5 = `
+The code bellow blablabla
+`;
 
 const data: any = {
   simpleStateManagement: {
-    title: 'React - Simple State Management',
+    title,
     text: [
       {
-        subtitle: 'Intro',
+        subtitle: subtitle1,
         paragraphsLinksCommandsCode: [ 
-          {
-            paragraph: 'The guide aims to explore the useState hook from react. We\'l build a Navbar that re-renders a list of items when hovered over. We\'l also create a form and a table, wich will share data by lifting the state up.'
-          },
+          { paragraph: paragraph1 },
         ] 
       },
       {
-        subtitle: 'Setup',
+        subtitle: subtitle2,
         paragraphsLinksCommandsCode: [ 
-          {
-            paragraph: 'Install the latest recommended version of'
-          },
-          {
-            paragraphWithEndLink: 'Install the latest recommended version of *Node.js'
-          },
-          {
-            paragraphWithMiddleLink: 'Install the latest recommended version of *Node.js* and in the future deno'
-          },
-          {
-            command: [
-              '$ npx create-react-app progrtmp-simple-state-management',
-              '$ cd progrtmp-simple-state-management',
-              '$ npm run start'
-            ]
-          },
-          {
-            paragraph: 'When "npm run start" finishes, React will lauch the app in http://localhost:3000'
-          },
+          { paragraphWithEndLink: paragraph2 },
+          { paragraphWithMiddleLink: paragraph3 },
+          { command: commands },
+          { paragraph: paragraph4 },
         ] 
       },
       {
-        subtitle: 'Navbar',
+        subtitle: subtitle3,
         paragraphsLinksCommandsCode: [
-          {
-            paragraph: 'The code bellow blablalba'
-          },
-          {
-            code: block1
-          }
+          { paragraph: paragraph5 },
+          { code: block1 }
         ]
       },
-    ]
-  }
+    ],
+  },
 };
 
 export default data;
