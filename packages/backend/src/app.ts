bfb2063bce52ef6b4ea0b9174fb3ee11463f78eb
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2021-01-29 11:18:24 -0300

    testing the backend graphql crud

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,20 +1,87 @@
 /* eslint-disable */
 
+import crypto from 'crypto';
 import express, { Request, Response, NextFunction } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-console.log('123');
+dotenv.config();
 
-const loggingMiddleware = (
-  req: Request, 
-  res: Response, 
-  next: NextFunction
-): void => {
-  console.log(`req = ${req}`);
-  next();
-};
+// const schema = buildSchema(`
+//   input MessageInput {
+//     content: String!
+//     author: String!
+//   }
+
+//   type Message {
+//     id: ID!
+//     content: String!
+//     author: String!
+//   }
+
+//   type Mutation {
+//     createMessage(input: MessageInput): Message
+//   }
+
+//   type Query {
+//     getMessage(id: ID!): Message
+//     getFixedMessage: String
+//   }
+// `);
+
+// const database: any = {};
+
+// const createMessage = ({ author, content}: any) => {
+//   const id = crypto.randomBytes(10).toString('hex');
+//   const dataToSave = {
+//     author,
+//     content,
+//     date: new Date()
+//   };
+//   database[id] = dataToSave;
+//   console.log(`Messaged saved. Id = ${id}. data=${JSON.stringify(dataToSave)}`);
+//   return { id, ...dataToSave };
+// };
+
+// const readMessage = (id: string) => {
+//   if (!database[id]) {
+//     throw new Error(`No message exists with id ${id}`);
+//   }
+//   return database[id];
+// };
+
+// const root = {
+//   createMessage: ({ input }: any) => createMessage(input),
+//   getMessage: ({ id }: any) => readMessage(id),
+//   getFixedMessage: () => `fixed msg`,
+// };
+
+// const app = express();
+// app.use(cors());
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+// }));
+// app.listen(process.env.PORT, () => {
+//   console.log(
+//     `Running a GraphQL API server at `
+//     + `${process.env.HOSTNAME}:${process.env.PORT}`
+//   );
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 var schema = buildSchema(`
   input MessageInput {
@@ -69,6 +136,7 @@ var root = {
     // Create a random id for our "database".
     id++; 
     fakeDatabase[id] = input;
+    console.log(`returned by class = ${JSON.stringify(new Message(id, input))}`) 
     return new Message(id, input);
   },
   updateMessage: ({id, input}: any) => {
@@ -86,11 +154,54 @@ var root = {
 
 var app = express();
 app.use(cors());
-app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
-app.listen(3001);
-console.log('Running a GraphQL API server at http://localhost:3001/graphql');
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Running a GraphQL API server at `
+    + `${process.env.HOSTNAME}:${process.env.PORT}`
+  );
+});
+
+/*
+post
+mutation {
+	createMessage(input: {
+		author: "author0",
+		content: "content0"
+  }) {
+		id
+	}
+}
+
+returns
+{
+  "data": {
+    "createMessage": {
+      "id": "1"
+    }
+  }
+}
+
+
+
+
+get
+query {
+	getMessage(id: "1") {
+	  id
+	}
+}
+
+{
+  "data": {
+    "getMessage": {
+      "id": "1"
+    }
+  }
+}
+*/
+
