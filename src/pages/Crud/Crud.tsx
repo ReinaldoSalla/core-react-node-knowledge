f1c71737d74954c1f5a5ba1b4a6c491c7e89a040
Reinaldo Salla <reinaldosalla95@gmail.com>
2021-05-29 12:06:47 -0300

    refactor: crud as an array of objects

--- src/pages/Crud/Crud.tsx
+++ src/pages/Crud/Crud.tsx
@@ -7,43 +7,87 @@ import {
 } from './Crud.styles';
 import Button from '../../shared/styles/Button.styles';
 
+// function Crud() {
+//   const [text, setText] = useState('Page Title');
+//   const [numSections, setNumSections] = useState(0);
+//   const [sectionsText, setSectionsText] = useState<string[]>([]);
+//   return (
+//     <Container>
+//       <Input
+//         type='text'
+//         value={text}
+//         onChange={(event) => setText(event.target.value)}
+//       />
+
+//       <ul>
+//         {new Array(numSections).fill(0).map((_, index) => (
+//           <li key={index.toString()}>
+//             <hr />
+//             <Input
+//               type='text'
+//               value={sectionsText[index]}
+//               onChange={(event) => {
+//                 const items = [...sectionsText];
+//                 items[index] = event.target.value;
+//                 setSectionsText(items);
+//               }}
+//             />
+//             <br />
+//             <Button>add a paragraph</Button>
+//             <Button>add code</Button>
+//           </li>
+//         ))}
+//       </ul>
+
+//       <hr />
+//       <Button onClick={() => {
+//         setNumSections(numSections + 1);
+//         setSectionsText(
+//          [...sectionsText, `Section title ${numSections + 1}`]
+//         );
+//       }}
+//       >
+//         add new section
+//       </Button>
+//     </Container>
+//   );
+// }
+
 function Crud() {
-  const [text, setText] = useState('Page Title');
-  const [numSections, setNumSections] = useState(0);
-  const [sectionsText, setSectionsText] = useState<string[]>([]);
+  const [sections, setSections] = useState<any[]>([]);
   return (
     <Container>
-      <Input
-        type='text'
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-
       <ul>
-        {new Array(numSections).fill(0).map((_, index) => (
+        {sections.map((section, index) => (
           <li key={index.toString()}>
             <hr />
             <Input
               type='text'
-              value={sectionsText[index]}
+              value={section.title}
               onChange={(event) => {
-                const items = [...sectionsText];
-                items[index] = event.target.value;
-                setSectionsText(items);
+                const items = [...sections];
+                items[index].title = event.target.value;
+                setSections(items);
               }}
             />
             <br />
+            <br />
             <Button>add a paragraph</Button>
             <Button>add code</Button>
+            <br />
+            <br />
           </li>
         ))}
       </ul>
 
-      <hr />
-      <Button onClick={() => {
-        setNumSections(numSections + 1);
-        setSectionsText([...sectionsText, `Section title ${numSections + 1}`]);
-      }}
+      <Button
+        onClick={() => {
+          const newSection = {
+            title: `section title ${sections.length + 1}`,
+            content: []
+          };
+          setSections([...sections, newSection]);
+        }}
       >
         add new section
       </Button>
